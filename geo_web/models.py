d2a253fce8b05445f2a13873765a4ae59ec9f0c0
VOUGY <guillaume.vouilloz@outlook.com>
2020-05-08 11:40:13 +0200

    Add Booking Models

--- geo_web/models.py
+++ geo_web/models.py
@@ -28,3 +28,17 @@ class Building(models.Model):
     id_0 = models.PositiveIntegerField(primary_key=True)
     geom = models.MultiPolygonField(srid=4326, null=True)
     id = models.BigIntegerField(null=True)
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    lastname = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=30)
+    email = models.CharField(max_length=60)
+    tel_number = models.CharField(max_length=30)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    busy = models.IntegerField()
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+
+
+
