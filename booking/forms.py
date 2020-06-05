f1b6231ba7b631b655c66fe28d8937b4fef6b0bb
Ken Jäggi <jaggi@icare.ch>
2020-06-05 10:29:15 +0200

    fix: resevation

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
 from .models import Booking
+from geo_web.models import Location
+from django import forms
 
 
 class BookForm(ModelForm):
+
+    location_id = forms.IntegerField()
+
     class Meta:
         model = Booking
-        fields = ['lastname', 'firstname', 'email', 'tel_number', 'date_start', 'date_end']
+        fields = ['lastname', 'firstname', 'email', 'tel_number', 'date_start', 'date_end', 'location_id']
+
+    def save(self, location_id=None):
+        booking = super(BookForm, self).save(commit=False)
+        booking.save()
+
+        location = Location.objects.get(id_0=self.cleaned_data['location_id'])
+        location.bookings.add(booking)
+        location.save()
+
+        return booking
